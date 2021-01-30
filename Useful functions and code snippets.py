413a5b95c6af764ebd0875f708b2e9687628a89b
xCuriousParanoiax <MuhamedRajaby_work@outlook.com>
2021-01-29 19:32:41 -0500

    Added line counters

---
+++ Useful functions and code snippets.py
@@ -0,0 +1,42 @@
+
+
+
+
+with open(targetFile) as f:	# Counts all lines
+	numberOfLines = len([l for l in f.readlines()])
+
+print(numberOfLines)
+
+
+
+
+
+
+with open(targetFile) as f:	# Excludes empty lines only
+	numberOfLines = len([l for l in f.readlines() if l.strip()])
+
+print(numberOfLines)
+
+
+
+
+
+
+with open(targetFile) as f:	# Excludes empty lines and lines who's first none empty character is a hashtag
+	numberOfLines = len([l for l in f.readlines() if l.strip() and not l.strip().startswith("#")])
+
+print(numberOfLines)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
